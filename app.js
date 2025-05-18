b02e959b2664530a2ea5acf61ac25b3889534456
clement Roland <clementroland52@gmail.com>
2025-05-18 00:49:42 -0400

    ajouter,modifier,supprimer et afficher

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+
+// recuper le schema sur lequel on va faire les requetes
+const Product = require('./models/modelDB');
+
 // connexion à la base de données
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://clement:roland@cluster0.8vhrmze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
@@ -18,24 +22,41 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+  
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+// afficher tous les produits depuis la base de données
+app.get('/api/products',(req, res, next) => {
+    Product.find()
+  .then(products=>res.status(200).json({products}))
+  .catch(error=>res.status(400).json({error}));
+});
+// recuperer les information d'un produit
+ app.get('/api/products/:id', (req, res, next) => {
+     Product.findOne({ _id: req.params.id })
+      .then(product => res.status(200).json(product))
+      .catch(error => res.status(400).json({ error }));
+  });
+// enregistrer dans la base de données
+app.post('/api/products',(req, res, next) => {
+  delete req.body._id;
+  const product = new Product({
+    ...req.body
+  });
+  product.save()
+  .then(product=>res.status(201).json({product}))
+  .catch((error)=>res.status(400).json({error}));
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+// modifier les inforamtions d'un produit
+app.put('/api/products/:id',(req, res, next) => {
+  Product.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+  .then(()=>res.status(201).json({message:'Modified!'}))
+  .catch(error=>res.status(400).json({error}));
 });
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
+// supprimer un produit
+app.delete('/api/products/:id',(req,res,next) =>{
+    Product.deleteOne({_id: req.params.id})
+    .then(()=>res.status(200).json({message:'Deleted!'}))
+    .catch(error=>res.status(400).json({error}));
+})
 module.exports = app;
--- models/modelDB.js
+++ models/modelDB.js
@@ -4,6 +4,6 @@ const productShema = mongoose.Schema({
     name : {type:String, require :true},
     description : {type:String, require :true},
     price : {type:String, require :true},
-    inStock : {type:String,require:true}
+    inStock : {type:Boolean,require:true}
 });
 module.exports = mongoose.model('product',productShema);
